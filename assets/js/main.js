ece6f9e444f49c88f56c218da1352693c75803f7
wic-epfl <wic.epfl@gmail.com>
2025-09-12 11:52:51 +0200

    Update main.js
    updated poptrox to select class=images only

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,17 +94,19 @@
 
 				if ($thumbs.length > 0)
 					$thumbs.poptrox({
-						onPopupClose: function() { $body.removeClass('is-covered'); },
-						onPopupOpen: function() { $body.addClass('is-covered'); },
-						baseZIndex: 10001,
-						useBodyOverflow: false,
-						overlayColor: '#222226',
-						overlayOpacity: 0.75,
-						popupLoaderText: '',
-						fadeSpeed: 500,
-						usePopupDefaultStyling: false,
-						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
+					    selector: 'a.image', // ✅ only <a class="image"> will trigger lightbox
+					    onPopupClose: function() { $body.removeClass('is-covered'); },
+					    onPopupOpen: function() { $body.addClass('is-covered'); },
+					    baseZIndex: 10001,
+					    useBodyOverflow: false,
+					    overlayColor: '#222226',
+					    overlayOpacity: 0.75,
+					    popupLoaderText: '',
+					    fadeSpeed: 500,
+					    usePopupDefaultStyling: false,
+					    windowMargin: (skel.breakpoint('small').active ? 5 : 50)
 					});
+
 
 			});
 
@@ -114,5 +116,6 @@
 			});
 
 	});
+
 
 })(jQuery);
